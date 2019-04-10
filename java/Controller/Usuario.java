0dd608b7910f3e5e5c3c33305e87d675f5597bfe
rails -v <rail -v>
2019-04-09 19:45:11 -0500

    Reorganizacion de los directorios

--- java/Controller/Usuario.java
+++ java/Controller/Usuario.java
@@ -1,57 +1,86 @@
-class Usuario {
+import java.util.Date;
 
-    private int usrDni;
-    private String usrNombre;
-    private String usrApellidos;
-    private String usrPassword;
-    private String usrMail;
-    private String usrCodigo;
+public class Usuario {
 
-    public void setUsrDni(int usrDni) {
-        this.usrDni = usrDni;
+   private String codUsuario;
+   private String nombre;
+   private String correo;
+   private long  dni;
+   private String contrasena;
+   private Date Fecha_nacimiento;
+   private String foto;
+
+    public Usuario(){}
+    public Usuario(String codUsuario,String nombre,String correo,long dni,String contrasena, Date Fecha_nacimiento, String foto){
+        this.codUsuario=codUsuario;
+        this.nombre=nombre;
+        this.correo=correo;
+        this.dni=dni;
+        this.contrasena=contrasena;
+        this.Fecha_nacimiento=Fecha_nacimiento;
+        this.foto=foto;
     }
 
-    public void setUsrNombre(String usrNombre) {
-        this.usrNombre = usrNombre;
+    public  void Set_CodUsuario(String codUsuario){
+        this.codUsuario=codUsuario;
+    }
+    public String Get_CodUsuario(){
+        return this.codUsuario;
     }
 
-    public void setUsrApellidos(String usrApellidos) {
-        this.usrApellidos = usrApellidos;
+    public void Set_Nombre(String nombre){
+        this.nombre=nombre;
+    }
+    public String Get_Nombre(){
+        return this.nombre;
     }
 
-    public void setUsrPassword(String usrPassword) {
-        this.usrPassword = usrPassword;
+    public void Set_Correo(String correo){
+        this.correo=correo;
+    }
+    public  String Get_Correo(){
+        return this.correo;
     }
 
-    public void setUsrMail(String usrMail) {
-        this.usrMail = usrMail;
+    public  void Set_DNI(long dni){
+        this.dni=dni;
+    }
+    public long Get_DNI(){
+        return this.dni;
     }
 
-    public void setUsrCodigo(String usrCodigo) {
-        this.usrCodigo = usrCodigo;
+    public String getContrasena() {
+        return contrasena;
     }
 
-    public int getUsrDni() {
-        return usrDni;
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
     }
 
-    public String getUsrNombre() {
-        return usrNombre;
+    public Date getFecha_nacimiento() {
+        return Fecha_nacimiento;
     }
 
-    public String getUsrApellidos() {
-        return usrApellidos;
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        Fecha_nacimiento = fecha_nacimiento;
     }
 
-    public String getUsrPassword() {
-        return usrPassword;
+    public String getFoto() {
+        return foto;
     }
 
-    public String getUsrMail() {
-        return usrMail;
+    public void setFoto(String foto) {
+        this.foto = foto;
     }
 
-    public String getUsrCodigo() {
-        return usrCodigo;
+    public void MensajeCumple(){
+        Date Fechaactual=new Date();
+        if(Fechaactual==getFecha_nacimiento()){
+            System.out.println("Feliz cumple");
+        }
     }
+
+
 }
+
+
