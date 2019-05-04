6b57be79b51e5842c7ae6fa1170b9d7e154bab1b
Diego Livia <diegolivia@hotmail.com>
2019-05-04 01:48:54 -0500

    del

--- RoffusModular/RoffusCore/src/main/java/com/myorg/business/PaqueteBusiness.java
+++
@@ -1,7 +0,0 @@
-package com.myorg.business;
-
-import com.myorg.model.entity.Paquete;
-
-public interface PaqueteBusiness extends CrudBusiness<Paquete> {
-    
-}
--- RoffusModular/RoffusCore/src/main/java/com/myorg/business/UsuarioBusiness.java
+++
@@ -1,7 +0,0 @@
-package com.myorg.business;
-
-import com.myorg.model.entity.Usuario;
-
-public interface UsuarioBusiness extends CrudBusiness<Usuario> {
-    
-}
